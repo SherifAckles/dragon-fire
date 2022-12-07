5442e936bfb6e893bf7566af9d73fbe35be94db6
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-12-07 15:45:21 -0500

    start building the game logic

--- game.js
+++ game.js
@@ -1,9 +1,18 @@
-let lastRenderTime= 0
-
+let lastRenderTime = 0;
+const DRAGON_SPEED = 1;
 // loop function
 function main(currentTime) {
-    window.requestAnimationFrame(main)
-    console.log(currentTime);
+  window.requestAnimationFrame(main);
+  const secondsLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsLastRender < 1 / DRAGON_SPEED) return;
+
+  console.log("Render"); //1 second update
+  lastRenderTime = currentTime;
+
+  // game logic
+  // update loop
+  update(); //Move the dragon to the correct position, make the dragon bigger or smaller, or lost the game
+  draw(); // take the logic from update, where rat needs to go
 }
 // run the loop for the first time
-window.requestAnimationFrame(main)
+window.requestAnimationFrame(main);
